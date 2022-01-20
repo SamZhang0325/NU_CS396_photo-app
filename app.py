9098889d303e42a1a87d855b147d0604ea1cea28
Pengxiang Zhang <z-git@protonmail.com>
2022-01-19 21:53:48 -0600

    Finish HW2

--- app.py
+++ app.py
@@ -4,22 +4,24 @@ import fake_data    # see fake_data.py file
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     current_user = fake_data.generate_user()
     return render_template(
-        'index.html', 
+        'index.html',
         user=current_user,
         posts=fake_data.generate_posts(n=8),
         stories=fake_data.generate_stories(n=6),
         suggestions=fake_data.generate_suggestions(n=7)
     )
 
+
 @app.route('/data')
 def data():
     current_user = fake_data.generate_user()
     return render_template(
-        'starter_template.html', 
+        'starter_template.html',
         user=current_user,
         posts=fake_data.generate_posts(n=8),
         stories=fake_data.generate_stories(n=6),
@@ -29,14 +31,18 @@ def data():
 #####################################
 # Just to get you thinking about    #
 # how to build a REST API (for HW3) #
-##################################### 
+#####################################
+
+
 @app.route('/api/feed')
 def get_feed():
     return json.dumps(fake_data.generate_posts(10))
 
+
 @app.route('/api/stories')
 def get_stories():
     return json.dumps(fake_data.generate_stories(5))
+
 
 @app.route('/api/suggestions')
 def get_suggestions():
@@ -47,6 +53,6 @@ def get_suggestions():
 # Enables flask app to run using    #
 # "python3 app.py" command.         #
 # Or you can use "flask run"        #
-##################################### 
+#####################################
 if __name__ == '__main__':
     app.run()
