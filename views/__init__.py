4bd60d39a077814b8e72fe67806f3d7e6b1f7937
Pengxiang Zhang <z-git@protonmail.com>
2022-01-30 23:49:44 -0600

    Add Unit Test

--- views/__init__.py
+++ views/__init__.py
@@ -1,18 +1,18 @@
 from models import db, Following, Post
+from sqlalchemy import and_
 
 '''
 Below are some helper functions to help you with security:
 '''
 
-
 def get_authorized_user_ids(current_user):
     # query the "following" table to get the list of authorized users:
     user_ids_tuples = (
         db.session
-        .query(Following.following_id)
-        .filter(Following.user_id == current_user.id)
-        .order_by(Following.following_id)
-        .all()
+            .query(Following.following_id)
+            .filter(Following.user_id == current_user.id)
+            .order_by(Following.following_id)
+            .all()
     )
     # convert to a list of ints:
     user_ids = [id for (id,) in user_ids_tuples]
@@ -21,16 +21,13 @@ def get_authorized_user_ids(current_user):
     user_ids.append(current_user.id)
     return user_ids
 
-
 def can_view_post(post_id, user):
     # find user_ids that the user can follow (including the user themselves)
     auth_users_ids = get_authorized_user_ids(user)
 
     # query for all the posts that are owned by the user:
-    # TODO: Bug here
-    post = Post.query.filter(Post.user_id.in_(
-        auth_users_ids)).filter(Post.id == post_id).first()
-    # post = Post.query.filter(Post.id==post_id and Post.user_id.in_(auth_users_ids)).first()
+    post = Post.query.filter(and_(Post.id==post_id, Post.user_id.in_(auth_users_ids))).first()
     if not post:
         return False
     return True
+        
