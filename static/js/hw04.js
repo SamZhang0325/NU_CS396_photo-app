4eea31d2801a245cc7854d829a85f618c46e45f7
Pengxiang Zhang <z-git@protonmail.com>
2022-02-13 21:45:06 -0600

    Create / Delete Data

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -40,14 +40,14 @@ const displayProfile = () => {
 
 const suggestions2Html = (suggestions) => {
   return `
-    <div class="suggestedUsers">
+    <div class="suggestedUsers" suggestion_id=${suggestions.id}>
 
     <img alt="${suggestions.username} Avatar" src="${suggestions.thumb_url}" />
     <div>
         <span class="username">${suggestions.username}</span><br>
         <span style="color: #585858; margin-right:30px">suggested for you</span>
     </div>
-    <a href="#" style="float:right;">follow</a>
+    <spam id="followButton"><a href="javascript:{}" style="float:right;" onclick="suggestedFollow(${suggestions.id},undefined)" aria-label="Follow this user">follow</a></spam>
 </div>
       `;
 };
@@ -61,19 +61,52 @@ const displaySuggestions = () => {
     });
 };
 
+const posts2comment = (posts) => {
+  let comment, comment_number;
+  comment_number = posts.comments.length;
+  comment_number === 1
+    ? (comment = `<p><span class="username">${
+        posts.comments[comment_number - 1].user.username
+      }</span> ${posts.comments[comment_number - 1].text}</p>`)
+    : comment_number >= 1
+    ? (comment = `<a href="#">&nbsp;&nbsp;View all ${comment_number} comments</a><p><span class="username">${
+        posts.comments[comment_number - 1].user.username
+      }</span> ${posts.comments[comment_number - 1].text}</p>`)
+    : (comment = `The post doesn't have a comment yet.`);
+  return comment;
+};
+
+const posts2liked = (posts) => {
+  let liked = "far",
+    likeID,
+    likeAriaLabel = "Like this post";
+  posts.current_user_like_id &&
+    ((liked = "fas red"),
+    (likeID = posts.current_user_like_id),
+    (likeAriaLabel = "Unlike this post"));
+  return `<a class="${liked} fa-heart fa-lg invisibleLink" aria-label="${likeAriaLabel}" onclick="likePost(${posts.id},${likeID})" href="javascript:{}"></a>`;
+};
+
+const posts2bookmarked = (posts) => {
+  let bookmarked = "far",
+    bookmarkID,
+    bookmarkAriaLabel = "Bookmark this post";
+  posts.current_user_bookmark_id &&
+    ((bookmarked = "fas"),
+    (bookmarkID = posts.current_user_bookmark_id),
+    (bookmarkAriaLabel = "Remove bookmark for this post"));
+  return `<a class="${bookmarked} fa-bookmark fa-lg invisibleLink" aria-label="${bookmarkAriaLabel}" onclick="bookmarkPost(${posts.id},${bookmarkID})" href="javascript:{}" style="float:right;"></a>
+    `;
+};
+
+const posts2likeCount = (posts) => {
+  return `<p class="likesCount">${posts.like_count} likes</p>`;
+};
+
 const posts2Html = (posts) => {
-  let liked = "far", likeID;
-  posts.current_user_like_id && (liked ="fas red", likeID=posts.current_user_like_id);
-  let bookmarked = "far", bookmarkID;
-  posts.current_user_bookmark_id && (bookmarked ="fas", bookmarkID=posts.current_user_bookmark_id);
-  let comment;
-  posts.comments.length === 1 ? (
-  comment = `<p><span class="username">${posts.comments[0].user.username}</span> ${posts.comments[0].text}</p>`
-  ): posts.comments.length >= 1 ? (
-    comment = `<a href="#">&nbsp;&nbsp;View all ${posts.comments.length} comments</a><p><span class="username">${posts.comments[0].user.username}</span> ${posts.comments[0].text}</p>`
-  ):(
-      comment = `The post doesn't have a comment yet.`
-  )
+  const liked = posts2liked(posts);
+  const bookmarked = posts2bookmarked(posts);
+  const comment = posts2comment(posts);
   return `
     <div class="card" post_id=${posts.id}>
         <div>
@@ -86,10 +119,10 @@ const posts2Html = (posts) => {
         <img alt="${posts.title}" src="${posts.image_url}" />
         <div>
             <p>
-                <a class="${liked} fa-solid fa-heart fa-lg invisibleLink" current_user_like_id=${likeID} aria-label="Like the Post" href="#"></a>&ensp;
+            <spam id="likeico">${liked}</spam>&ensp;
                 <a class="far fa-comment fa-lg invisibleLink" aria-label="View Comments" href="#"></a>&ensp;
                 <a class="far fa-paper-plane fa-lg invisibleLink" aria-label="Share" href="#"></a>
-                <a class="${bookmarked} fa-bookmark fa-lg invisibleLink" current_user_bookmark_id=${bookmarkID} aria-label="Save" href="#" style="float:right;"></a>
+                <spam id="bookmarkico">${bookmarked}</spam>
             </p>
             <p class="likesCount">${posts.like_count} likes</p>
             <p>
@@ -103,9 +136,12 @@ const posts2Html = (posts) => {
         <span class="post_time">${posts.display_time}</span>
         <div class="make_comments">
             <div class="make_comments_inside">
+            <form onsubmit="submitComment(event);return false;" id="form-${posts.id}">
                 <i class="far fa-smile"></i>
-                <input type="text" class="make_comments_hint" aria-label="Input your comment" placeholder="Add a comment..."> </input>
-                <a href="#" style="float:right;">Post</a>
+                <input type="text" name="comment" class="make_comments_hint" aria-label="Input your comment" placeholder="Add a comment..." required> </input>
+                <input type="hidden" name=post_id value=${posts.id}> </input>
+                <input type="submit" class="commentSubmit" href="#" style="" value="Post">
+                </form>
             </div>
         </div>
     </div>
@@ -133,5 +169,145 @@ const initPage = () => {
   }, 100);
 };
 
-// invoke init page to display stories:
 initPage();
+
+const likePost = (post_id, current_user_like_id) => {
+  const html = document
+    .querySelector(`[post_id="${post_id}"]`)
+    .querySelector("#likeico");
+  const likeHtml = document
+    .querySelector(`[post_id="${post_id}"]`)
+    .querySelector(".likesCount");
+  const likeCount = parseInt(likeHtml.textContent.replace(/\D/g, ""));
+  if (current_user_like_id) {
+    fetch(`/api/posts/${post_id}/likes/${current_user_like_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        html.innerHTML = `<a class="far fa-heart fa-lg invisibleLink" aria-label="Like this post" onclick="likePost(${post_id},undefined)" href="javascript:{}"></a>`;
+        likeHtml.innerHTML = `${likeCount - 1} likes`;
+      });
+  } else {
+    fetch(`/api/posts/${post_id}/likes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        html.innerHTML = `<a class="fas red fa-heart fa-lg invisibleLink" aria-label="Unlike this post" onclick="likePost(${post_id},${data.id})" href="javascript:{}"></a>`;
+        likeHtml.innerHTML = `${likeCount + 1} likes`;
+      });
+  }
+};
+
+const bookmarkPost = (post_id, current_user_bookmark_id) => {
+  const html = document
+    .querySelector(`[post_id="${post_id}"]`)
+    .querySelector("#bookmarkico");
+  if (current_user_bookmark_id) {
+    fetch(`/api/bookmarks/${current_user_bookmark_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        html.innerHTML = `<a class="far fa-bookmark fa-lg invisibleLink" aria-label="Bookmark this post" onclick="bookmarkPost(${post_id},undefined)" href="javascript:{}" style="float:right;"></a>`;
+      });
+  } else {
+    const postData = {
+      post_id: post_id,
+    };
+
+    fetch("/api/bookmarks/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        html.innerHTML = `<a class="fas fa-bookmark fa-lg invisibleLink" aria-label="Remove bookmark from this post" onclick="bookmarkPost(${post_id},${data.id})" href="javascript:{}" style="float:right;"></a>`;
+      });
+  }
+};
+
+const suggestedFollow = (suggestion_id, followingID) => {
+  const html = document
+    .querySelector(`[suggestion_id="${suggestion_id}"]`)
+    .querySelector("#followButton");
+  if (followingID) {
+    fetch(`/api/following/${followingID}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        html.innerHTML = `<a href="javascript:{}" style="float:right;" onclick="suggestedFollow(${suggestion_id},undefined)" aria-label="follow this user">follow</a>`;
+      });
+  } else {
+    const postData = {
+      user_id: suggestion_id,
+    };
+
+    fetch("/api/following/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        html.innerHTML = `<a href="javascript:{}" style="float:right;" onclick="suggestedFollow(${suggestion_id},${data.id})" aria-label="Unfollow this user">unfollow</a>`;
+      });
+  }
+};
+
+const submitComment = (event) => {
+  const post_id = event.target.elements.post_id.value;
+  const comment = event.target.elements.comment.value;
+  const postData = {
+    post_id: post_id,
+    text: comment,
+  };
+  fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      fetch(`http://127.0.0.1:5000/api/posts/${post_id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const html = posts2comment(data);
+          document
+            .querySelector(`[post_id="${post_id}"]`)
+            .querySelector(".comments").innerHTML = html;
+        });
+    });
+  document
+    .querySelector(`[post_id="${post_id}"]`)
+    .querySelector(`#form-${post_id}`)
+    .reset();
+};
